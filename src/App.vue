e49001ce77f8ae2b8a2090657627447eb1c2851a
jonathan <jonathancastro2018@yahoo.com>
2019-12-06 02:14:46 +0100

    creando componente home

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+    <Home/>   
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  //hay que importar todos los compenentes aqui
+//import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  components: {   
+    Home
   }
 }
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h1>{{nombre}}</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data(){
+    return{
+      nombre: 'nombre'
+    }
+  }
+  
+
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
